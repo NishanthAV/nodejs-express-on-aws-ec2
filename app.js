db8fa8837d2f364a73b166132db7e2061a3c6c62
NishanthAV <nishanthdaredevil1@gmail.com>
2022-05-18 17:08:56 +0530

    fixes

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Portfolio Generated</h4> <p>Version 3.0</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
 app.get('/products', (req, res) => {
   res.send([
     {
       productId: '101',
-      price: 100
+      productname: 'SG BAT',
+      price: 2000
     },
     {
       productId: '102',
-      price: 150
+      productname: 'Vicky Tennis Ball',
+      price: 80
+    },
+    {
+        productId: '103',
+        productname: 'Yonex Raquet',
+        price: 2999
+    },
+    {
+        productId: '104',
+        productname: 'Fastrack watch',
+        price: 1800
+    },
+    {
+        productId:'105',
+        productname: 'Playstation 4',
+        price:27999
+    },
+    {
+        productId: '106',
+        productname:'Yezdi Scrambler',
+        price: 274719
     }
   ])
 })
@@ -22,4 +44,3 @@ app.get('/products', (req, res) => {
 app.listen(port, ()=> {
   console.log(`Demo app is up and listening to port: ${port}`);
 })
- 
